108777bec842ac81ebfc0767dbd3c7643010fb8a
Deniro <deniro.gayle@mymona.uwi.edu>
2018-04-30 04:52:08 +0000

    profile change

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python flask-migrate.py db upgrade --directory migration
 web: gunicorn -w 4 -b "0.0.0.0:$PORT" app:app
--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,9 @@ from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:1234@localhost/project2db" # needs to be modified
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:1234@localhost/project2db" # needs to be modified
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # some kind of warning suppression
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jywbykmldqworr:ded89bd565bccfacf40b47c867ac8ba0594d45dd3d72f5a50066af83440ad4d2@ec2-23-23-142-5.compute-1.amazonaws.com:5432/ddir1kqhhc1ap4'
 
 app.config['UPLOAD_FOLDER'] = join(dirname(realpath(__file__)), 'static/uploads/') # user image upload folder location
 app.config['POSTS_FOLDER'] = join(dirname(realpath(__file__)), 'static/posts/') # posts image upload folder location
